8b22355fba7f748d85c6ffc1b2eb0ee12efbe90d
Barry Lowe <Barry.Lowe@hermes-europe.co.uk>
2021-07-16 00:57:01 +0100

    Refactored based on item name

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
@@ -7,34 +8,47 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            #item.sell_in = item.sell_in - 1
+            if item.name == "Aged Brie":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+                if item.sell_in < 0:
+                    item.quality = item.quality + 1
+                if item.quality > 50:
+                    item.quality = 50
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                # increase by 1, 2 or 3 depending on sell_in value
+                if item.sell_in > 10:
+                    item.quality = item.quality + 1
+                elif item.sell_in <= 10 and item.sell_in > 5:
+                    item.quality = item.quality + 2
+                elif item.sell_in <= 5 and item.sell_in > 0:
+                    item.quality = item.quality + 3 
+                elif item.sell_in <= 0:
+                    item.quality = 0
+                # reset quality to 50 if it goes above 50
+                if item.quality > 50:
+                    item.quality = 50
+            elif item.name == "Conjured Mana Cake":
+                if item.quality > 0:
+                    item.quality = item.quality - 2
+                if item.quality < 0:
+                    item.quality = 0
+            elif item.name == "Elixir of the Mongoose":
+                if item.quality > 0:
+                    item.quality = item.quality - 1
+                if item.sell_in < 0:
+                    item.quality = item.quality - 1
+                if item.quality < 0:
+                    item.quality = 0
+            elif item.name == "+5 Dexterity Vest":
+                if item.quality > 0:
+                    item.quality = item.quality - 1
+                if item.quality < 0:
+                    item.quality = 0
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                item.quality = 80
+            item.sell_in = item.sell_in - 1
 
 class Item:
     def __init__(self, name, sell_in, quality):
